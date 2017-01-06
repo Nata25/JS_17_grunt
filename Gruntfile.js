ea55e575b66b8b4258737ca19d0ed7d90cb11fd8
Natalya Ivanova <natalyas.ivanova@gmail.com>
2017-01-06 15:37:55 +0200

    Minify (uglify) .js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,25 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      concat: {
-          build: {
-              src: "src/js/*.js",
-              dest: "js/script.js"
-          }
-      }
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            build: {
+                src: "src/js/*.js",
+                dest: "js/script.js"
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'js/script.min.js': ['js/*.js']
+                }
+            }
+        }
     });
 
-    // Load the plugin that provides the "concat" task.
+    // Load plugins for default tasks
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    // Default task(s).
-    grunt.registerTask('default', ['concat']);
+    // Default tasks
+    grunt.registerTask('default', ['concat', 'uglify']);
 };
